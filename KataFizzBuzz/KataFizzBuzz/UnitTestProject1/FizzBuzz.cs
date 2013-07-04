0580746b84ceacd2cbe3f6dbe1593b2e4a987150
Yoann Ramos <yramos@epsi.fr>
2013-07-04 22:45:28 +0200

    Base

---
+++ KataFizzBuzz/KataFizzBuzz/UnitTestProject1/FizzBuzz.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTestProject1
+{
+    class FizzBuzz
+    {
+        public FizzBuzz() { }
+
+        public List<String> listeDesCentsPremiersFizzBuzz() 
+        {
+            List<string> listeDesCentsPremiersFizzBuzz = new List<string>();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i == 3)
+                {
+                    listeDesCentsPremiersFizzBuzz.Add("Fizz");
+                }
+                else if (i == 5)
+                {
+                    listeDesCentsPremiersFizzBuzz.Add("Buzz");
+                }
+                else
+                {
+                    listeDesCentsPremiersFizzBuzz.Add(i.ToString());
+                }
+                
+            }
+            return listeDesCentsPremiersFizzBuzz;
+        }
+        
+    }
+}
---
+++ KataFizzBuzz/KataFizzBuzz/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TestFizzBuzz
+    {
+        private FizzBuzz _monFizzBuzz = new FizzBuzz();
+
+        [TestMethod]
+        public void TestFizzBuzz_peutObtenirLesCentPremiersNombres()
+        {
+            List<String> listeDesCentsPremiersFizzBuzz = _monFizzBuzz.listeDesCentsPremiersFizzBuzz();
+
+            Assert.IsNotNull(listeDesCentsPremiersFizzBuzz);
+            Assert.AreEqual(100, listeDesCentsPremiersFizzBuzz.Count);
+        }
+
+        [TestMethod]
+        public void TestFizzBuzz_troisRetourneFizz()
+        { 
+            List<String> listeDesCentsPremiersFizzBuzz = _monFizzBuzz.listeDesCentsPremiersFizzBuzz();
+
+            Assert.AreEqual("Fizz", listeDesCentsPremiersFizzBuzz[2]);//l'indice commence à 0
+        }
+
+        [TestMethod]
+        public void TestFizzBuzz_cinqRetourneBuzz() 
+        {
+            List<string> listeDesCentsPremiersFizzBuzz = _monFizzBuzz.listeDesCentsPremiersFizzBuzz();
+
+            Assert.AreEqual("Buzz", listeDesCentsPremiersFizzBuzz[4]);
+        }
+
+        [TestMethod]
+        public void TestFizzBuzz_nombreDivisibleParTroisRetourneFizz() 
+        {
+            List<string> listeDesCentsPremiersFizzBuzz = _monFizzBuzz.listeDesCentsPremiersFizzBuzz();
+
+            Assert.AreEqual("Fizz", listeDesCentsPremiersFizzBuzz[5]);
+            Assert.AreEqual("Fizz", listeDesCentsPremiersFizzBuzz[11]);
+            Assert.AreEqual("Fizz", listeDesCentsPremiersFizzBuzz[17]);
+        }
+    }
+}
